df2839b7b86f8c1c523ca1eed8d7c4cb02b6898f
Peter-G-Stone <peter.stone.online@gmail.com>
2018-10-25 15:56:42 -0400

    Done.

---
+++ Rakefile
@@ -0,0 +1,6 @@
+require_relative 'bin/environment'
+
+desc 'Loads up a console!'
+task :console do
+  Pry.start
+end
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,36 @@
 class Pokemon
+
+    @@all = []
+
+    attr_accessor :id, :name, :type, :db, :hp
+
+    def initialize (id = nil, name = nil, type = nil, db = nil)
+        @id = id
+        @name = name 
+        @type = type
+        @db = db
+        @hp = hp
+    end
+
+    def self.save(tempname, temptype, db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type) VALUES(?, ?)
+
+        SQL
+        db.execute(sql, tempname, temptype)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find(tempid, db)
+        sql = <<-SQL 
+            SELECT * FROM pokemon
+            WHERE id = ?
+        SQL
+        result = db.execute(sql, tempid).first
+
+        self.new(result[0], result[1], result[2], db)
+    end
 end
