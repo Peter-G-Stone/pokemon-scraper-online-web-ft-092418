5c5dd580c5f43d3434c0f00b10d8ccb107418c05
Peter-G-Stone <peter.stone.online@gmail.com>
2018-10-25 16:57:48 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,7 +4,7 @@ class Pokemon
 
     attr_accessor :id, :name, :type, :db, :hp
 
-    def initialize (id = nil, name = nil, type = nil, db = nil)
+    def initialize (id = nil, name = nil, type = nil, db = nil, hp = 60)
         @id = id
         @name = name 
         @type = type
@@ -25,12 +25,21 @@ class Pokemon
     end
 
     def self.find(tempid, db)
+        key = "id"
         sql = <<-SQL 
             SELECT * FROM pokemon
-            WHERE id = ?
+            WHERE #{key} = ?
         SQL
-        result = db.execute(sql, tempid).first
+        result = db.execute(sql, tempid).first        
+        self.new(result[0], result[1], result[2], db, result[3])
+    end
 
-        self.new(result[0], result[1], result[2], db)
+    def alter_hp(hpIn, db)
+        key = "hp"
+        tempID = self.id
+        sql = <<-SQL
+            UPDATE pokemon SET hp = ? WHERE id = #{tempID}
+        SQL
+        db.execute(sql, hpIn)
     end
 end
